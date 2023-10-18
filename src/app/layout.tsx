4bccaa5b14de2d7cde01aaf67206eb46acdd9155
c1334690 <joao.ribeiro@capgemini.com>
2023-10-18 10:19:03 -0300

    feat(tn02-sidebar/logo): implementa sidebar com a logo do sistema

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Sidebar } from '@/components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="relative min-h-screen dark:bg-zinc-900 lg:grid lg:grid-cols-app">
+          <Sidebar />
+        </div>
+        <main className="max-w-screen px-4 pb-12 pt-24 lg:col-start-2 lg:w-auto lg:px-8 lg:pt-8">
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,9 @@
+import { Logo } from "./Logo";
+
+export function Sidebar() {
+  return (
+    <div className="flex items-center justify-between">
+      <Logo />
+    </div>
+  )
+}
